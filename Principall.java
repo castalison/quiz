dec3d06835cc27d9e14e1854305be2534be38bf7
Alison Castillo <acastillo60614@ufide.ac.cr>
2025-07-16 20:47:56 -0600

    Add files via upload

---
+++ Principall.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package principal;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aliso
+ */
+public class Principall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //pedi toda la informacion necesaria
+        String nombreSuper = JOptionPane.showInputDialog("por favor digame el nombre del supermercado");
+        String codigoSuper = JOptionPane.showInputDialog("por favor digame el codigo del supermercado");
+        String nombreEmpleado = JOptionPane.showInputDialog("por favor digame el nombre del empleado");
+        String cedulaEmpleado = JOptionPane.showInputDialog("por favor digame la cedula del empleado");
+        int cantidadProductos = JOptionPane.showInputDialog("por favor digame la cantidad de productos");
+    }
+
+}
